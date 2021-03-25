8a7cf8e65cf4c5ada291a1619d8914ed7c740ef8
Ifera <iferareaves@gmail.com>
2021-03-26 03:06:23 +0500

    added comments

--- core/functions/util_functions.php
+++ core/functions/util_functions.php
@@ -1,7 +1,8 @@
 <?php
 declare(strict_types = 1);
 
-function checkInput($str): string{
+// strips the special character, spaces, slashes from the argument
+function stripInput($str): string{
 	$str = htmlspecialchars($str);
 	$str = trim($str);
 	$str = stripslashes($str);
@@ -9,15 +10,18 @@ function checkInput($str): string{
 	return $str;
 }
 
+// proceed to the home page of the website
 function home(): void{
 	header("Location: " . BASE_URL);
 	exit();
 }
 
+// show an alert via js
 function _alert(string $msg): void{
 	echo '<script type = "text/javascript"> alert("' . $msg . '") </script>';
 }
 
+// log stuff to console
 function _log(string $msg): void{
 	echo '<script type = "text/javascript"> console.log("' . $msg . '") </script>';
 }
@@ -26,9 +30,11 @@ function isConnected(): bool{
 	return isset($_SESSION["connected"]);
 }
 
-function disconnect(): void{
+// disconnect the user from the db and destroy the session if needed
+function disconnect(bool $destroySession = true): void{
 	DB::DESTROY();
-	session_destroy();
+	unset($_SESSION["connected"]);
+	if($destroySession) session_destroy();
 }
 
 // To be used for paths that could be accessed via web
@@ -36,15 +42,17 @@ function _resource(string $path): string{
 	return "/" . URI_NAME . "/" . $path;
 }
 
+// returns the url to the desired path prefixed with the websites address
 function _url(string $path): string{
 	return BASE_URL . $path;
 }
 
-// Particular use case involves inclusions
+// particular use case involves inclusions..
 function _path(string $path): string{
 	return APP_PATH . $path;
 }
 
+// head over to another page
 function _go(string $path): void{
 	header("Location: " . BASE_URL . $path);
 	exit();
